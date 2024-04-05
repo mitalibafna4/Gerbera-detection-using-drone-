85cb0ca1cfbbb0be09ef6eaaed0e25fc6c963a9c
Mitali Bafna <80626622+mitalibafna4@users.noreply.github.com>
2024-04-05 09:46:25 +0530

    Update 2_Object_Detection_-_Roboflow_Train.py

--- pages/2_Object_Detection_-_Roboflow_Train.py
+++ pages/2_Object_Detection_-_Roboflow_Train.py
@@ -12,7 +12,7 @@ from roboflow import Roboflow
 
 # Set default values
 DEFAULT_PROJECT_URL = "https://app.roboflow.com/kkwagh-63ouy/kkwagh-group15/1"
-DEFAULT_PRIVATE_API_KEY = "VXt2m4b0MAmxKtIYMcHZ"
+DEFAULT_PRIVATE_API_KEY = "C8Izi0EdivGuDkEYVXGB"
 
 # Initialize session state with default values
 if 'project_url_od' not in st.session_state:
